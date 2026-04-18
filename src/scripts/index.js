3a363aaccc20389c0c076a8d4e7c3c2437beef1b
Emiliano Pisu <pisuemiliano.1980@gmail.com>
2026-04-18 09:07:07 +0200

    feat(slides): finalize slides

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import "@pixu-talks/core";
 import "./baseline-status/baseline-status.js";
 
 import "./_doodle.js";
+import { initFpsBasics } from "./_00-fps-basics.js";
 
 import { initSlideHooks } from "./_slide-hooks.js";
 import { initTiltLayeredCard } from "./_08-tilt-layered-card.js";
@@ -12,6 +13,7 @@ import { initTiltLayeredCard } from "./_08-tilt-layered-card.js";
  */
 
 document.addEventListener("DOMContentLoaded", () => {
+	initFpsBasics();
 	initSlideHooks();
 	initTiltLayeredCard();
 });
